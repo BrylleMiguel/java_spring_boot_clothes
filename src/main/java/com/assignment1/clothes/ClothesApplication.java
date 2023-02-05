bf47a09e42ec1bdb9c567b72f7a5a6087d093a79
brylle <miguelbrylle@gmail.com>
2023-02-05 18:29:50 -0500

    Initial commit

---
+++ src/main/java/com/assignment1/clothes/ClothesApplication.java
@@ -0,0 +1,14 @@
+package com.assignment1.clothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesApplication.class, args);
+
+		System.out.println("Hello World");
+	}
+}
